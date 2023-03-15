311971661f1fb2b65977e84bc66873d5feb6625a
Khalidprithy <khalidprithy@gmail.com>
2023-03-15 18:46:19 +0600

    Password error fixed

--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,39 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
-const { verifyTokenAndAdmin } = require('./verifyAuth');
+const { verifyTokenAndAdmin, verifyToken } = require('./verifyAuth');
 
 const router = require('express').Router();
 
-router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+router.put("/:id", verifyToken, async (req, res) => {
     if (req.body.password) {
         const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        res.body.password = hashedPassword;
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hashedPassword;
     }
     try {
         const updatedUser = await User.findByIdAndUpdate(req.params.id,
             { $set: req.body },
             { new: true });
-        res.status(200).json(updatedUser)
+        res.status(200).json(updatedUser);
     } catch (error) {
-        res.status(500).json(error)
+        res.status(500).json(error);
     }
+})
 
+router.put("admin/:id", verifyTokenAndAdmin, async (req, res) => {
+    if (req.body.password) {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPasswordAdmin = await bcrypt.hash(password, salt);
+        res.body.password = hashedPasswordAdmin;
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,
+            { $set: req.body },
+            { new: true });
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 })
 
 module.exports = router
